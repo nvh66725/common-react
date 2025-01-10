2f1202440aa4b5cfb3ad22384b1a184ae8eea9dd
= <nvh66725@gmail.com>
2025-01-10 22:48:28 +0700

    add file deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "pulling code ..."
+git pull origin master
+echo "installing ..."
+yarn
+node --max-old-space-size4096 node_modules/react-scripts/scripts/build.js
+echo "building ..."
+yarn build
+echo "Deploying to server ..."
+sudo cp -r build/* /var/www/html
+echo "Deployed successfully!"
